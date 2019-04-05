dd27dbb207dfe9849246874a874bf4b1495a678b
Won Kim <wonsae.kim@gmail.com>
2019-04-05 17:01:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(str)
   res = ""
   str.split(" ").each_with_index do |c, i|
     res += c.reverse
-    if !c.equal?(str[-1])
+    if i != str.length - 1
       res += " "
     end
   end
